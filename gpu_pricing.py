d8588227bbd517d06d23d3ae13d19d5a193a8142
Vaishnavii4 <vaishnavip0409@gmail.com>
2025-02-13 18:51:18 +0530

    Update gpu_pricing.py

--- gpu_pricing.py
+++ gpu_pricing.py
@@ -1,11 +1,11 @@
 import re
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 from babel.numbers import format_currency
 import streamlit as st
 import requests
